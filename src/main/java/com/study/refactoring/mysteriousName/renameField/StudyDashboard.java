9f61c9375f5946d079e688ac52ebcad9b32c9074
DrumJ <tmdgh717@naver.com>
2025-02-17 15:36:59 +0900

    리팩토링 3. 필드 이름 바꾸기

---
+++ src/main/java/com/study/refactoring/mysteriousName/renameField/StudyDashboard.java
@@ -0,0 +1,43 @@
+package com.study.refactoring.mysteriousName.renameField;
+
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHIssueComment;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+import org.kohsuke.github.GitHubBuilder;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class StudyDashboard {
+
+    //필드명 변경 usernames -> reviewers
+    //private Set<String> reviewers = new HashSet<>();
+    //private Set<String> reviews = new HashSet<>();
+
+    //Record 클래스 사용
+    private Set<StudyReview> studyReviews = new HashSet<>();
+
+    private void loadReviews() throws IOException {
+        GitHub gitHub = new GitHubBuilder().withOAuthToken("").build();
+        GHRepository repository = gitHub.getRepository("whiteship/live-study");
+        GHIssue issue = repository.getIssue(30);
+
+        List<GHIssueComment> reviews = issue.getComments();
+        for (GHIssueComment review : reviews) {
+            studyReviews.add(new StudyReview(review.getUser().getName(), review.getBody()));
+        }
+    }
+
+    public Set<StudyReview> getStudyReviews() {
+        return studyReviews;
+    }
+
+    public static void main(String[] args) throws IOException {
+        StudyDashboard studyDashboard = new StudyDashboard();
+        studyDashboard.loadReviews();
+        studyDashboard.getStudyReviews().forEach(System.out::println);
+    }
+}
