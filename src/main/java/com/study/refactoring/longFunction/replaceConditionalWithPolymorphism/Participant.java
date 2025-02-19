c19dd6f6966ef2a4bce64b39d13f578c37e777a0
승호 <tmdgh717@naver.com>
2025-02-19 15:16:49 +0900

    리팩토링 13 전

---
+++ src/main/java/com/study/refactoring/longFunction/replaceConditionalWithPolymorphism/Participant.java
@@ -0,0 +1,21 @@
+package com.study.refactoring.longFunction.replaceConditionalWithPolymorphism;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public record Participant(String username, Map<Integer, Boolean> homework) {
+    public Participant(String username) {
+        this(username, new HashMap<>());
+    }
+
+    public double getRate(double total) {
+        long count = this.homework.values().stream()
+                .filter(v -> v == true)
+                .count();
+        return count * 100 / total;
+    }
+
+    public void setHomeworkDone(int index) {
+        this.homework.put(index, true);
+    }
+}
---
+++ src/main/java/com/study/refactoring/longFunction/replaceConditionalWithPolymorphism/PrinterMode.java
@@ -0,0 +1,5 @@
+package com.study.refactoring.longFunction.replaceConditionalWithPolymorphism;
+
+public enum PrinterMode {
+    CONSOLE, CVS, MARKDOWN
+}
