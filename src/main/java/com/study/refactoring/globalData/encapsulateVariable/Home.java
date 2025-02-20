1f3a455fc8b4f8056f8f1bde3f08d483c95984d3
승호 <tmdgh717@naver.com>
2025-02-20 13:18:53 +0900

    리팩토링 17. 변수 캡슐화하기

--- src/main/java/com/study/refactoring/globalData/encapsulateVariable/Home.java
+++ src/main/java/com/study/refactoring/globalData/encapsulateVariable/Home.java
@@ -3,8 +3,8 @@ package com.study.refactoring.globalData.encapsulateVariable;
 public class Home {
 
     public static void main(String[] args) {
-        System.out.println(Thermostats.targetTemperature);
-        Thermostats.targetTemperature = 68;
-        Thermostats.readInFahrenheit = false;
+        System.out.println(Thermostats.getTargetTemperature());
+        Thermostats.setTargetTemperature(68);
+        Thermostats.setReadInFahrenheit(false);
     }
 }
