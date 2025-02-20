c56ba0ca692228898958e988e5ecac0b64042b53
승호 <tmdgh717@naver.com>
2025-02-20 12:33:58 +0900

    리팩토링 14 전

---
+++ src/main/java/com/study/refactoring/longParameterList/replaceParameterWithQuery/Order.java
@@ -0,0 +1,23 @@
+package com.study.refactoring.longParameterList.replaceParameterWithQuery;
+
+public class Order {
+
+    private int quantity;
+
+    private double itemPrice;
+
+    public Order(int quantity, double itemPrice) {
+        this.quantity = quantity;
+        this.itemPrice = itemPrice;
+    }
+
+    public double finalPrice() {
+        double basePrice = this.quantity * this.itemPrice;
+        int discountLevel = this.quantity > 100 ? 2 : 1;
+        return this.discountedPrice(basePrice, discountLevel);
+    }
+
+    private double discountedPrice(double basePrice, int discountLevel) {
+        return discountLevel == 2 ? basePrice * 0.90 : basePrice * 0.95;
+    }
+}
