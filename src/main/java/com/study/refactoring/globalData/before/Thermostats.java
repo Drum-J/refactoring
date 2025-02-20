59b8b99ad2c1c3ad6eb6a5124796ba0aad4759ed
승호 <102205699+Drum-J@users.noreply.github.com>
2025-02-20 13:28:03 +0900

    05.전역 데이터 (#5)
    * 리팩토링 17. 변수 캡슐화하기

---
+++ src/main/java/com/study/refactoring/globalData/before/Thermostats.java
@@ -0,0 +1,15 @@
+package com.study.refactoring.globalData.before;
+
+public class Thermostats {
+
+    public static Integer targetTemperature = 70;
+
+    public static Boolean heating = true;
+
+    public static Boolean cooling = false;
+
+    public static Boolean fahrenheit = true;
+
+
+
+}
