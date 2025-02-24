4bbb7695ec8bb3b3b1218298c8de68f1df13157e
승호 <tmdgh717@naver.com>
2025-02-24 10:18:10 +0900

    리팩토링 29. 클래스 인라인

--- src/test/java/com/study/refactoring/shotgunSurgery/inlineClass/ShipmentTest.java
+++ src/test/java/com/study/refactoring/shotgunSurgery/inlineClass/ShipmentTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class ShipmentTest {
     @Test
     void trackingInfo() {
-        Shipment shipment = new Shipment(new TrackingInformation("UPS", "12345"));
+        Shipment shipment = new Shipment("UPS", "12345");
         assertEquals("UPS: 12345", shipment.getTrackingInfo());
     }
 }
