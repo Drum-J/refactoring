7f61ad9f3d40a6dbd1f99bd91ea9d4a12afb07d3
승호 <tmdgh717@naver.com>
2025-02-24 10:10:45 +0900

    리팩토링 29 전

---
+++ src/main/java/com/study/refactoring/shotgunSurgery/inlineClass/Shipment.java
@@ -0,0 +1,22 @@
+package com.study.refactoring.shotgunSurgery.inlineClass;
+
+public class Shipment {
+
+    private TrackingInformation trackingInformation;
+
+    public Shipment(TrackingInformation trackingInformation) {
+        this.trackingInformation = trackingInformation;
+    }
+
+    public TrackingInformation getTrackingInformation() {
+        return trackingInformation;
+    }
+
+    public void setTrackingInformation(TrackingInformation trackingInformation) {
+        this.trackingInformation = trackingInformation;
+    }
+
+    public String getTrackingInfo() {
+        return this.trackingInformation.display();
+    }
+}
